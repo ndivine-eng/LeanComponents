a342d78df546c928422049a0b69815d05668c43f
ndivine-eng <144015345+ndivine-eng@users.noreply.github.com>
2024-02-22 13:13:08 +0200

    done with task

---
+++ screen/Search.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, label, money } from 'react-native';
+
+export default function Search({ title, image, price, text, picture, drip,label , money }) {
+  return (
+    
+      <View style={styles.clothes}>
+        <Text style={{ fontSize: 15 , color:'white', position:'absolute', top:0, left:0}}>{title}</Text>
+        <Image source={{ uri: `${image}` }} style={styles.img} />
+        <View style={{ display: 'flex', flexDirection: 'row', backgroundColor:'rgba(0, 0, 0, 0.5)',width:'90%', justifyContent: 'space-between', position:'absolute', bottom:0, left:0, height:'25%', borderBottomEndRadius:20, borderBottomStartRadius:20}}>
+          <Text style={{color:'white'}}>{price}</Text>
+          <Text>{text}</Text>
+          
+        </View>
+      </View>
+
+      
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  clothes: {
+    width:200,
+    height:150,
+    position: 'relative',
+  
+  
+  
+  },
+ 
+  img: {
+    width: '90%', 
+    height: '100%',
+    borderRadius: 20,
+  
+  },
+});
